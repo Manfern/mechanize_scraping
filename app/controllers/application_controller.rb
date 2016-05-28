08f54bfba4c80f6d5cc6f7e07fd0c729f04155b4
Sergei Sharapov <ejilur@gmail.com>
2016-05-28 10:17:22 +0300

    Installed linkedin scrape gem and created new action in app controller #scrape_now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  require 'linkedin-scraper'
+  def scrape_now
+  	'scraping'
+	  profile = Linkedin::Profile.new("http://www.linkedin.com/in/jeffweiner08")
+	  
+  end
+
 end
